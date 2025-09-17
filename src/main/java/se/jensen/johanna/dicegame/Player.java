b0041383164a270d68a1418c872d2ad8e9ef3a18
johannajonsson <jojo.risin@outlook.com>
2025-09-17 13:19:52 +0200

    Tog bort isvalidname metoden i player, då den inte behövs för har allt sådant i ui. Tog även bort att setfirstname och lastname throws exception då jag redan har dessa metoder i main.

--- src/main/java/se/jensen/johanna/dicegame/Player.java
+++ src/main/java/se/jensen/johanna/dicegame/Player.java
@@ -7,33 +7,22 @@ public class Player{
     private int totalScore;
 
 
-    public void setFirstName(String firstName) throws IllegalArgumentException{
-        if(!isValidName(firstName)){
-            throw new IllegalArgumentException("Invalid input. Please fill in all required names");
-        }else{
-            this.firstName=firstName;
-        }
+    public void setFirstName(String firstName) {
+
+        this.firstName=firstName;
 
     }
-    public void setLastName(String lastName) throws IllegalArgumentException{
-        if(!isValidName(lastName)){
-            throw new IllegalArgumentException("Please enter a Name");
-        }else{
-            this.lastName=lastName;
-        }
+    public void setLastName(String lastName){
+        this.lastName=lastName;
 
     }
 
-    private boolean isValidName(String name){
-        return name!=null && !name.trim().isEmpty();
-
-    }
 
     public String getFirstName(){
         return firstName;
     }
     public String getLastName(){
-        return lastName; //oanvänd av helt personliga skäl.
+        return lastName;
     }
     public String getFullName(){
         return firstName+" "+lastName;
